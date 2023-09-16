b7df49539b7cb154b616e500d72c1c5052bcf245
Kamrul Islam <121850714+KamrulJS@users.noreply.github.com>
2023-09-16 16:31:37 +0000

    container config done

---
+++ Basic JS/1-let-cons-var.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+let accountEmail = "account@gmail.com";
+var accountPassword = "mk12345";
+
+
+// accountId = 132164
+// accountEmail = "sdmns@gmail.com";
+// accountPassword = "sd123"
+
+// dont use "var" variable
+
+console.table([accountId, accountEmail, accountPassword]);
+
+
+
+
